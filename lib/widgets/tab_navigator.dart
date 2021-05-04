6c1fc4019914d5869e64debe20bdfb08e4a17535
Lakshitha <lakshitha_deshapriya@yahoo.com>
2021-05-04 22:52:01 +0530

    Finalized tabs and colors

--- lib/widgets/tab_navigator.dart
+++ lib/widgets/tab_navigator.dart
@@ -2,27 +2,35 @@ import 'package:auto_parts/constants/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nav_bar_widget/nav_bar_widget.dart';
+import 'package:widget_lib/widgets/utils/widget_util.dart';
 
 class TabNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PlatformNavBarScaffold(
       forceMaterial: Constant.forceMaterial,
+      activeColor: WidgetUtil.isDarkMode(context)
+          ? CupertinoColors.white
+          : CupertinoColors.systemBlue,
       barItems: [
         BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.chat_bubble_2_fill),
-          label: 'Parts test',
+          icon: Icon(CupertinoIcons.wrench),
+          activeIcon: Icon(CupertinoIcons.wrench_fill),
+          label: 'Parts',
         ),
         BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.bookmark_fill),
+          icon: Icon(Icons.home_repair_service_outlined),
+          activeIcon: Icon(Icons.home_repair_service),
           label: 'Services',
         ),
         BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.rectangle_grid_2x2_fill),
+          icon: Icon(CupertinoIcons.bookmark),
+          activeIcon: Icon(CupertinoIcons.bookmark_fill),
           label: 'Favourites',
         ),
         BottomNavigationBarItem(
-          icon: Icon(CupertinoIcons.settings_solid),
+          icon: Icon(CupertinoIcons.settings),
+          activeIcon: Icon(CupertinoIcons.settings_solid),
           label: 'Settings',
         ),
       ],
