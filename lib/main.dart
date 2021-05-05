80b792996207c035872bd4990091ac7857b2dc85
Lakshitha <lakshitha_deshapriya@yahoo.com>
2021-05-05 22:23:14 +0530

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:auto_parts/constants/constant.dart';
 import 'package:auto_parts/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:widget_lib/widget_lib.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
