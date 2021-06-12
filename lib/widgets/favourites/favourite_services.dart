51ff5b1bb9be1edbad5f6b5e5536f75cb9e5d79f
Lakshitha <lakshitha_deshapriya@yahoo.com>
2021-06-13 00:28:28 +0530

    Favourite service list view

--- lib/widgets/favourites/favourite_services.dart
+++ lib/widgets/favourites/favourite_services.dart
@@ -1,6 +1,10 @@
+import 'package:auto_parts/providers/favourite_provider.dart';
 import 'package:auto_parts/widgets/common/app_bar_title_with_search.dart';
+import 'package:auto_parts/widgets/services/service_item.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tuple/tuple.dart';
 import 'package:widget_lib/widget_lib.dart';
 
 class FavouriteServices extends StatelessWidget {
@@ -10,6 +14,9 @@ class FavouriteServices extends StatelessWidget {
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
 
+    final FavouriteProvider favouriteProvider =
+        context.read<FavouriteProvider>();
+
     return PlatformScaffold(
       topPadding: false,
       appBar: AppBar(
@@ -18,8 +25,47 @@ class FavouriteServices extends StatelessWidget {
       cupertinoAppBar: CupertinoNavigationBar(
         middle: AppBarTitleWithSearch(onSearchSubmitted: onSearchTextSubmitted),
       ),
-      body: Center(
-        child: ThemeText('No Favourite Services'),
+      body: Selector<FavouriteProvider, Tuple2<bool, String>>(
+        selector: (_, provider) =>
+            Tuple2(provider.isInitialized, provider.newFavouritePartId),
+        builder: (_, tuple, __) {
+          if (tuple.item1) {
+            if (favouriteProvider.favouriteParts.isEmpty) {
+              return Center(
+                child: ThemeText(
+                  'No Favourite Services',
+                  style: TextStyle(
+                    fontSize: width * 0.055,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              );
+            }
+            return Padding(
+              padding: EdgeInsets.only(
+                top: width * 0.01,
+                bottom: width * 0.01,
+              ),
+              child: ListView.builder(
+                itemCount: favouriteProvider.favouriteServices.length,
+                itemBuilder: (context, index) {
+                  return ServiceItem(
+                    width: width,
+                    service: favouriteProvider.favouriteServices[index],
+                    showDate: false,
+                    enableFavouriteAction: true,
+                  );
+                },
+              ),
+            );
+          } else {
+            return Center(
+              child: PlatformCircularProgressIndicator(
+                height: width * 0.3,
+              ),
+            );
+          }
+        },
       ),
     );
   }
